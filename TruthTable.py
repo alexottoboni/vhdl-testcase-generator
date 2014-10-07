55e07d333fd9cb060e2dd10b3e840bd30a929304
Alex Ottoboni <alexottoboni@gmail.com>
2014-10-06 23:38:44 -0700

    Smarter formatting

--- TruthTable.py
+++ TruthTable.py
@@ -3,46 +3,58 @@ import sys
 
 class TruthTable:
     
-    def __init__(self):
-        self.truth_table_inputs = OrderedDict()
-        self.truth_table_outputs = OrderedDict()
+    def __init__(self, inputs = [], outputs = [], expression = None):
+        self.expression = ""
+        self.inputs = OrderedDict()
+        self.outputs = OrderedDict()
+        self.create_col_headers(inputs, outputs)
+        self.fill_inputs()
 
-    def create_input_headers(self, inputs):
+    def create_col_headers(self, inputs, outputs):
         for input in inputs:
-            self.truth_table_inputs[input] = []
-
-    def get_num_inputs(self):
-        return len(self.truth_table_inputs.keys())
-
+            self.inputs[input] = []
+        for output in outputs:
+            self.outputs[output] = []
+    
     def fill_inputs(self):
-        inputs = self.truth_table_inputs.keys()
-
+        inputs = self.inputs.keys()
         num_inputs = self.get_num_inputs() 
-
         for i in range(num_inputs):
             for k in range(0, pow(2, i)):
                 for j in range(0, pow(2, num_inputs)/(2*(pow(2, i)))):
-                    self.truth_table_inputs[inputs[i]].append(False)
+                    self.inputs[inputs[i]].append(False)
                 for j in range(0, pow(2, num_inputs)/(2*(pow(2, i)))):
-                    self.truth_table_inputs[inputs[i]].append(True)
+                    self.inputs[inputs[i]].append(True)
+
+    def get_num_inputs(self):
+        return len(self.inputs.keys())
+
+    def get_longest_key_len(self):
+        keys = self.inputs.keys() + self.outputs.keys()
+        return len(sorted(keys, key = len)[-1])
 
     def display(self):
-        inputs = self.truth_table_inputs.keys()
+        inputs = self.inputs.keys()
+        outputs = self.outputs.keys()
+        longest_key_len = self.get_longest_key_len()
         for title in inputs:
-            sys.stdout.write("%-6s" % title + " ")
+            sys.stdout.write("%-*s" % (longest_key_len, title) + " ")
+        for title in outputs:
+            sys.stdout.write("%-*s" % (longest_key_len, title) + " ")
         sys.stdout.write("\n")
-
         num_rows = pow(2, self.get_num_inputs())
         for row in range(0, (num_rows)):
             for value in inputs:
-                sys.stdout.write('%-6s' %  str(self.truth_table_inputs[value][row]) + " ")
+                if (self.inputs[value][row] == True):
+                    sys.stdout.write('%-*s' % (longest_key_len,"1") + " ")
+                else:
+                    sys.stdout.write('%-*s' % (longest_key_len,"0") + " ")
             sys.stdout.write("\n")
 
 if __name__ == "__main__":
-    truth_table = TruthTable()
-    inputs = ['a', 'b', 'c', 'd']
-    truth_table.create_input_headers(inputs)
-    truth_table.fill_inputs()
+    inputs = ['a', 'b', 'c']
+    outputs = ['sum']
+    truth_table = TruthTable(inputs, outputs)
     truth_table.display()
         
         

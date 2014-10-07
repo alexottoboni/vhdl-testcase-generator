a6c95a41587667d5fcf2dee140c7da68be0e012b
Alex Ottoboni <alexottoboni@gmail.com>
2014-10-07 00:38:15 -0700

    Writes simple VHDL to output.vhd

--- TruthTable.py
+++ TruthTable.py
@@ -42,19 +42,65 @@ class TruthTable:
         for title in outputs:
             sys.stdout.write("%-*s" % (longest_key_len, title) + " ")
         sys.stdout.write("\n")
+
+        combined_inputs_outputs = (inputs + outputs)
         num_rows = pow(2, self.get_num_inputs())
         for row in range(0, (num_rows)):
-            for value in inputs:
-                if (self.inputs[value][row] == True):
-                    sys.stdout.write('%-*s' % (longest_key_len,"1") + " ")
-                else:
-                    sys.stdout.write('%-*s' % (longest_key_len,"0") + " ")
+            for value in combined_inputs_outputs:
+            
+                if value in inputs:
+                    sys.stdout.write('%-*s' % (longest_key_len, str(int(self.inputs[value][row]))))
+                    sys.stdout.write(" ")
+               
+                if value in outputs:
+                    sys.stdout.write('%-*s' % (longest_key_len, str(int(self.outputs[value][row]))))
+                    sys.stdout.write(" ")
             sys.stdout.write("\n")
 
+    def fill_outputs(self):
+        num_inputs = self.get_num_inputs()
+        outputs = self.outputs.keys()
+
+        for output in outputs:
+            sys.stdout.write("Filling: " + output + "\n") 
+            for i in range(0, pow(2, num_inputs)):
+                is_valid = False
+                while not is_valid:
+                    new_value = raw_input()
+                    if (new_value.upper() == "TRUE") or (new_value == "1") or (new_value.upper() == "T"):
+                        self.outputs[output].append(True)
+                        is_valid = True
+                    elif (new_value.upper() == "FALSE") or (new_value == "0") or (new_value.upper() == "F"):
+                        self.outputs[output].append(False)
+                        is_valid = True
+                    else:
+                        print "Invalid input, must be: true, false, 1, or 0"
+                    
+            
+            
+    def output_vhdl(self):
+        output_file = open("./output.vhd", 'w')
+        output_file.write("\n")
+        num_inputs = self.get_num_inputs()
+        for i in range(0, pow(2, num_inputs)):
+            output_file.write("\t\t-- Test #" + str(i + 1) + "\n")
+            for input in self.inputs.keys():
+                output_file.write("\t\t" + input + " <= " + "'" + str(int(self.inputs[input][i])) + "';" + "\n")
+            output_file.write("\n")
+            output_file.write("\t\t" + "wait for 10 ns;\n")
+            output_file.write("\n")
+            for output in self.outputs.keys():
+                output_file.write("\t\t" + "assert " + output + " = " + "'" + str(int(self.outputs[output][i])) + "' ")
+                output_file.write("report \"Failed test " + str(i + 1) + "\"" + ";" + "\n")
+            output_file.write("\n")
+
+
 if __name__ == "__main__":
-    inputs = ['a', 'b', 'c']
-    outputs = ['sum']
+    inputs = ['A', 'B', 'C']
+    outputs = ['Sum']
     truth_table = TruthTable(inputs, outputs)
+    truth_table.fill_outputs()
     truth_table.display()
+    truth_table.output_vhdl()
         
         
